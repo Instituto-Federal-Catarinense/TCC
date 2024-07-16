a1aae588d0c577a590430b6a45f89eb50d453540
dani980boo <daniel980boo@gmail.com>
2024-07-16 16:38:54 -0300

    trocaParaEjs

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -41,5 +41,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bem-Vindo' });
+  res.render('index',{ title: 'Bem-Vindo' });
 });
 
 router.get('/sobreNos', function(req, res, next) {
