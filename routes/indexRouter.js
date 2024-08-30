c45765b413c5a8a84f709a315a0cf2f8e6223c4b
dani980boo <daniel980boo@gmail.com>
2024-08-30 11:49:59 -0300

    rotas treinos e playlists arrumadas

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,27 +2,27 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index',{ title: 'Bem-Vindo' });
 });
 
-router.get('/sobreNos', function(req, res, next) {
+router.get('/sobreNos', function(req, res) {
   res.render('sobreNos', { title: 'Sobre Nós' });
 });
 
-router.get('/treino', function(req, res, next) {
-  res.render('treino', {title: 'Treino'});
+router.get('/treinos', function(req, res) {
+  res.render('treinos', {title: 'Treino'});
 });
 
-router.get('/perfil', function(req, res, next) {
+router.get('/pefil', function(req, res) {
   res.render('perfil', {title: 'Perfil'});
 });
 
-router.get('/cadastro', function(req, res, next) {
+router.get('/cadastro', function(req, res) {
   res.render('cadastro', {title: 'Cadastro'});
 });
 
-router.get('/playlist', function(req, res, next){
+router.get('/playlist', function(req, res){
   res.render('playlist', {title: 'playlist'});
 });
 module.exports = router;
